a715b633c87056f9c53dc0ecdcd4512457c9e8c9
JAIROPEREIRA11 <105813968+JAIROPEREIRA11@users.noreply.github.com>
2025-07-08 14:26:05 -0300

    .

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,21 @@
-// Substitua pelas suas credenciais do Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBzDAsthRk_vKng59u1vHUBelxbxhT4-Aw",
+  authDomain: "jogo-educativo-86afb.firebaseapp.com",
+  projectId: "jogo-educativo-86afb",
+  storageBucket: "jogo-educativo-86afb.firebasestorage.app",
+  messagingSenderId: "409472042344",
+  appId: "1:409472042344:web:be702da9bc1e6970cd42dd",
+  measurementId: "G-CZ4930P5KM"
 };
-// Inicialização do Firebase
-firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
